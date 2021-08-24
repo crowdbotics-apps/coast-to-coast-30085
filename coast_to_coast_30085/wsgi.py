46fbcc23ca278b1afe50c67e32fb78aa191aeafa
Crowdbotics <team@crowdbotics.com>
2021-08-24 21:59:32 +0000

    Initial commit.

---
+++ coast_to_coast_30085/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for coast_to_coast_30085 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coast_to_coast_30085.settings')
+
+application = get_wsgi_application()
